2eb31b4edfe28660bbd17e183c3b087ed4a97c71
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-07-27 12:45:20 +0300

    Завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryItemsMurkup = galleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galleryItemsMurkup);
+
+gallery.addEventListener("click", onClickItemGallery);
+
+function galleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onClickItemGallery(evn) {
+  evn.preventDefault();
+
+  const isLink = evn.target.classList.contains("gallery__image");
+  if (!isLink) {
+    return;
+  }
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionType: "attr",
+    captionPosition: "bottom",
+    captionDelay: 250,
+    captionsData: "alt",
+    docClose: true,
+  });
+
+  lightbox.on("show.simplelightbox");
+}
