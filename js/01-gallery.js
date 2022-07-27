e02c6d49d87446e32335f456cc01f30cbae926e5
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-07-27 09:44:02 +0300

    Завдання 1 маркап галлереї

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+import * as basicLightbox from "basiclightbox";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryItemsMurkup = galleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galleryItemsMurkup);
+
+gallery.addEventListener("click", onClickItemGallery);
+
+function galleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onClickItemGallery(evn) {
+  evn.preventDefault();
+
+  const isLink = evn.target.classList.contains("gallery__image");
+  if (!isLink) {
+    return;
+  }
+  const imgClick = evn.target.dataset.source;
+
+  modalShow(imgClick);
+}
+
+function modalShow(a) {
+  console.log(a);
+  const instance = basicLightbox.create(`
+    <img src="${a}" width="800" height="600">
+`);
+
+  instance.show();
+}
